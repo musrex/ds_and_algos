c26298a8d3ae457c5bbd80d834f25265e8d22b06
musrex <leandro.n.lopez@gmail.com>
2023-03-26 13:19:56 -0400

    week2 stack flow control

--- week2/main.py
+++ week2/main.py
@@ -23,18 +23,42 @@ def readKids(filename):
 
 def main():
     #filename = input("Enter filename: ")
-    readKids("Lopez_Family_Farm.csv")
+    kids = readKids("Lopez_Family_Farm.csv")
+    
+    # start up a while loop so we can repeatedly prompt user
     run = True
     while run:
-        toRemove = input("Enter the number (1-4) of items to remove: ")
-        try:
-            toRemove = int(toRemove)
-            for num in range(toRemove):
-                s.pop()
-            # NEEDS WORK
-            #for k in kids:
-            #    if s[-1] == k:
-            #        print(k)
-        except:
-            input("Please enter a number in the range of 1-4. Press Enter to continue.")
+        print(s)
+        # exit loop and quit program once stack is empty
+        if len(s) == 0:
+            print("No more names in the stack")
+            run = False
+            break
+        else:
+            # we take user input and then 
+            toRemove = input("Enter the number (1-4) of items to remove: ")
+            try:
+                # in a try block conver
+                toRemove = int(toRemove)
+                if toRemove in range(1,5):
+                    for num in range(toRemove):
+                        s.pop()
+                    # if the user inputs a number higher than the number of items in the list
+                    # we use the number of items as the input and end the program
+                    if len(s) < toRemove:
+                        for num in range(len(s)):
+                            s.pop()
+                            print("No more names in the stack")
+                            run = False
+                            break
+                    # this gets the key from the top of the stack and uses it to retrieve our person object.
+                    else:
+                        for k in kids:
+                            if s[-1] == k:
+                                print(k,kids[k].getFullName(),kids[k].getAge())
+                                print(len(s))
+                else:
+                    input("Please enter a number in the range of 1-4. Press Enter to continue.")
+            except:
+                input("Please enter a number in the range of 1-4. Press Enter to continue.")
 main()
