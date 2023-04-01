ed6149464606e2c0ccdd012eb5b276f2e0d5a0c2
musrex <leandro.n.lopez@gmail.com>
2023-04-01 18:50:52 -0400

    starting week 3 graph

---
+++ week3/graph.py
@@ -0,0 +1,25 @@
+class Graph:
+    def __init__(self, edges):
+        self.edges = edges
+        self.graph_dict = {}
+        for start, end in self.edges:
+            if start in self.graph_dict:
+                pass
+            else:
+
+if __name__ == "__main__":
+    routes = [
+        ("Mumbai", "Paris"),
+        ("Mumbai", "Dubai"),
+        ("Paris", "Dubai"),
+        ("Paris", "New York"),
+        ("Dubai", "New York"),
+        ("New York", "Toronto"),
+    ]
+
+    route_graph = Graph(routes)
+
+    d = {
+    "Mumbai": ["Paris","Dubai"],
+    "Paris" : ["Dubai", "New York"]
+    }
